3cf4ecf2de31148f2e0bf828f9659f79b970ac5c
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2019-06-24 20:17:59 -0700

    Update controls_updater.sh

--- controls_updater.sh
+++ controls_updater.sh
@@ -1,2 +1,10 @@
-cd && cd /opt/retropie/configs/all && wget -N https://raw.githubusercontent.com/CrashCortez/Gpi/master/runcommand-onend.sh && wget - N https://raw.githubusercontent.com/CrashCortez/Gpi/master/runcommand-onstart.sh && sudo chmod a+x *.sh 
-cd && cd /opt/retropie/supplementary/xboxdvr/bin && wget -N https://raw.githubusercontent.com/CrashCortez/Gpi/master/quit.sh && sudo chmod a+x quit.sh
+cd
+cd /opt/retropie/configs/all
+wget -N https://raw.githubusercontent.com/CrashCortez/Gpi/master/runcommand-onend.sh
+wget - N https://raw.githubusercontent.com/CrashCortez/Gpi/master/runcommand-onstart.sh
+sudo chmod a+x *.sh
+
+cd
+cd /opt/retropie/supplementary/xboxdvr/bin
+wget -N https://raw.githubusercontent.com/CrashCortez/Gpi/master/quit.sh
+sudo chmod a+x quit.sh
