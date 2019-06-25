88c50feaa836994386585ca45039eb652266ef46
SinisterSpatula <49018201+SinisterSpatula@users.noreply.github.com>
2019-06-24 23:42:56 -0500

    Delete controls_updater_test.sh

--- controls_updater_test.sh
+++
@@ -1,2 +0,0 @@
-cd && cd /opt/retropie/configs/all && wget -N https://raw.githubusercontent.com/CrashCortez/Gpi/test/runcommand-onend.sh && wget - N https://raw.githubusercontent.com/CrashCortez/Gpi/test/runcommand-onstart.sh && sudo chmod a+x *.sh 
-cd && cd /opt/retropie/supplementary/xboxdvr/bin && wget -N https://raw.githubusercontent.com/CrashCortez/Gpi/test/quit.sh && sudo chmod a+x quit.sh
