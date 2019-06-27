4279570800402c8a5a63b586e75cc7b434665a5c
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-06-26 22:41:31 -0500

    Fix LB and RB, change to LB+Y=Tab and RB+Y=Enter
    Fix LB and RB, change to LB+Y=Tab and RB+Y=Enter

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-#select+start=quit, A and B are turbo if held, LB and RB has secondary function if held.
+#select+start=quit, LB and RB has secondary function with y button hold.
 if [ "$2" = "mame4all" ]
 then
     sudo /opt/retropie/supplementary/xboxdrv/bin/xboxdrv \
@@ -17,8 +17,8 @@ then
     --evdev-keymap BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,BTN_TL=lb,BTN_TR=rb,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_MODE=guide,BTN_SELECT=back,BTN_START=start,BTN_TRIGGER_HAPPY3=du,BTN_TRIGGER_HAPPY4=dd,BTN_TRIGGER_HAPPY1=dl,BTN_TRIGGER_HAPPY2=dr \
     --evdev-absmap ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y \
     --ui-buttonmap start=KEY_1,back=KEY_5,back+start=KEY_ESC \
-    --ui-buttonmap a=KEY_LEFTCTRL,b=KEY_LEFTALT,x=KEY_SPACE,y=KEY_LEFTSHIFT,lb=KEY_Z:KEY_TAB:900,rb=KEY_X:KEY_ENTER:900 \
-    --ui-buttonmap du=KEY_UP,dd=KEY_DOWN,dl=KEY_LEFT,dr=KEY_RIGHT \
+    --ui-buttonmap a=KEY_LEFTCTRL,b=KEY_LEFTALT,x=KEY_SPACE,y=KEY_LEFTSHIFT,lb=KEY_Z,rb=KEY_X \
+    --ui-buttonmap du=KEY_UP,dd=KEY_DOWN,dl=KEY_LEFT,dr=KEY_RIGHT,y+lb=KEY_TAB,y+rb=KEY_ENTER \
 &
 fi
 
