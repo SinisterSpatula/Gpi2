b6c189163490051466894b20c90e47815eac2c2f
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-06-29 00:00:36 -0500

    Update runcommand-onstart.sh

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+#quit (select+start), requires /etc/udev/rules.d/99-xboxdrv.rules
+if  [ "$2" = "snes9x" ]
+then
+    sudo /opt/retropie/supplementary/xboxdrv/bin/xboxdrv \
+    --evdev /dev/input/by-id/usb-RetroFlag_GPi_Case_RetroFlag_01-event-joystick \
+    --detach-kernel-driver \
+    --silent \
+    --force-feedback \
+    --deadzone-trigger 15% \
+    --deadzone 4000 \
+    --mimic-xpad \
+    --dpad-as-button \
+    --evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_RX=x2,ABS_RY=y2,ABS_Z=lt,ABS_RZ=rt \
+    --evdev-keymap BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,BTN_TL=lb,BTN_TR=rb,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_MODE=guide,BTN_SELECT=back,BTN_START=start,BTN_TRIGGER_HAPPY3=du,BTN_TRIGGER_HAPPY4=dd,BTN_TRIGGER_HAPPY1=dl,BTN_TRIGGER_HAPPY2=dr \
+    --evdev-absmap ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y \
+    --ui-buttonmap x=KEY_E,a=KEY_D,b=KEY_S,lb=KEY_A,Y=KEY_W,rb=KEY_F \
+    --ui-buttonmap start=KEY_SPACE,back=KEY_LEFTCTRL,back+start=KEY_ESC \
+    --ui-buttonmap du=KEY_UP,dd=KEY_DOWN,dl=KEY_LEFT,dr=KEY_RIGHT \
+&
+fi
+
 
 #select+start=quit, LB and RB has secondary function with y button hold.
 if [ "$2" = "mame4all" ]
