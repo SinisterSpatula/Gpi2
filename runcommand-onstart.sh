1d77d8181c39d715365a22c89cf3569403c56ef2
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-07-07 03:18:39 -0500

    Update runcommand-onstart.sh

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -15,8 +15,8 @@ then
     --evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_RX=x2,ABS_RY=y2,ABS_Z=lt,ABS_RZ=rt \
     --evdev-keymap BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,BTN_TL=lb,BTN_TR=rb,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_MODE=guide,BTN_SELECT=back,BTN_START=start,BTN_TRIGGER_HAPPY3=du,BTN_TRIGGER_HAPPY4=dd,BTN_TRIGGER_HAPPY1=dl,BTN_TRIGGER_HAPPY2=dr \
     --evdev-absmap ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y \
-    --ui-buttonmap x=KEY_RIGHTCTRL,a=void,b=void,y=void,lb=KEY_A,rb=KEY_D \
-    --ui-buttonmap start=KEY_ENTER,back=KEY_ESC,back+start=KEY_F10 \
+    --ui-buttonmap x=KEY_RIGHTCTRL,a=void,b=KEY_SPACE+KEY_E,y=KEY_RIGHTSHIFT,lb=KEY_A,rb=KEY_D \
+    --ui-buttonmap start=KEY_ENTER+KEY_M,back=KEY_ESC,back+start=KEY_F10 \
     --ui-buttonmap du=KEY_UP+KEY_W,du+a=KEY_HOME,a+du=KEY_HOME,dd=KEY_DOWN+KEY_S,dd+a=KEY_END,a+dd=KEY_END,dl=KEY_LEFT,dr=KEY_RIGHT \
 &
 fi
