3440fb2f215fa739131d538afe1d6b88224993f3
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-07-07 19:30:49 -0500

    Update runcommand-onstart.sh

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#Experimental Duke Nukem
+#Duke Nukem
 if [ "$2" = "eduke32" ]
 then
     sudo /opt/retropie/supplementary/xboxdrv/bin/xboxdrv \
@@ -12,6 +12,7 @@ then
     --deadzone 4000 \
     --mimic-xpad \
     --dpad-as-button \
+    --no-extra-events \
     --evdev-absmap ABS_X=x1,ABS_Y=y1,ABS_RX=x2,ABS_RY=y2,ABS_Z=lt,ABS_RZ=rt \
     --evdev-keymap BTN_SOUTH=a,BTN_EAST=b,BTN_NORTH=x,BTN_WEST=y,BTN_TL=lb,BTN_TR=rb,BTN_THUMBL=tl,BTN_THUMBR=tr,BTN_MODE=guide,BTN_SELECT=back,BTN_START=start,BTN_TRIGGER_HAPPY3=du,BTN_TRIGGER_HAPPY4=dd,BTN_TRIGGER_HAPPY1=dl,BTN_TRIGGER_HAPPY2=dr \
     --evdev-absmap ABS_HAT0X=dpad_x,ABS_HAT0Y=dpad_y \
