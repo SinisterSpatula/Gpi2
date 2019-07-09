9ee7f1ab3a82a7d802f749903f3e3893db1a173b
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-07-09 15:50:37 -0500

    Update xboxdrvstart.sh

--- xboxdrvstart.sh
+++ xboxdrvstart.sh
@@ -45,7 +45,7 @@ eduke32="--ui-buttonmap x=KEY_RIGHTCTRL,a=KEY_E,b=KEY_SPACE,y=KEY_LEFTSHIFT,lb=K
     --ui-buttonmap du=KEY_UP+KEY_W,du+a=rel:REL_Y:-5:5,a+du=rel:REL_Y:-5:5,dd=KEY_DOWN+KEY_S,dd+a=rel:REL_Y:5:5,a+dd=rel:REL_Y:5:5,dl=KEY_LEFT,dr=KEY_RIGHT \
     --ui-buttonmap start+y=KEY_J,start+b=KEY_N,start+x=KEY_H,start+a=KEY_TAB"
 
-openbor="--ui-buttonmap x=KEY_F,a=KEY_S,b=KEY_D,Y=KEY_A,lb=KEY_L,rb=KEY_R \
+openbor="--ui-buttonmap x=KEY_Z,a=KEY_D,b=KEY_A,Y=KEY_S+KEY_F,lb=KEY_L,rb=KEY_R \
     --ui-buttonmap start=KEY_ENTER,back=KEY_BACKSPACE,back+start=KEY_ESC \
     --ui-buttonmap du=KEY_UP,dd=KEY_DOWN,dl=KEY_LEFT,dr=KEY_RIGHT"
 
